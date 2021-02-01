0176ef2b2581ea6431a26b7c414dd808f26cf75b
Richard Brinkley <brinkley908@gmail.co.uk>
2021-02-01 10:34:46 +0000

    Update GetData to check for failure and return structured result

--- ResourceService/ClientApp/src/io.js
+++ ResourceService/ClientApp/src/io.js
@@ -11,8 +11,23 @@
 
 
     const response = await fetch(process.env.REACT_APP_MFE_RESOURCE + "/" + url, { headers: headers });
-    const data = await response.json();
-    return data;
+    console.log(response);
+
+    var results = {
+        ok: response.ok,
+        status: response.status,
+        statusText: response.statusText,
+        type: response.type,
+        url: response.url,
+        headers: response.headers,
+        response: null
+    }; 
+
+    if (response.ok) {
+        results.response = await response.json();
+    }
+
+    return results;
 }
 
 export default GetData;
